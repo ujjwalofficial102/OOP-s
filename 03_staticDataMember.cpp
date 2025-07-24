46954f796c53c01fd0adf81f3c3babc8d08fcdf4
ujjwalofficial102 <ujjwalmishra102@gmail.com>
2025-07-25 02:46:04 +0530

    static function member

--- 03_staticDataMember.cpp
+++ 03_staticDataMember.cpp
@@ -4,31 +4,46 @@ using namespace std;
 class Customer{
     string name;
     int acc_num, balance;
-
+    static int totalCust, totalBalance;
+    
 public:
-    static int totalCust;
 
     Customer(string name, int acc_num, int balance){
         this->name = name;
         this->acc_num = acc_num;
         this->balance = balance;
         totalCust++;
+        totalBalance += balance;
     }
+    // To access total customer even if it is private
+    // static function
+    static void getStaticData(){
+        cout << "Total Customer: " << totalCust << endl;
+        cout << "Total Balance: " << totalBalance << endl;
+    }
+
     void display(){
-        cout << name << " " << acc_num << " " << balance << " " << totalCust << endl;
+        cout << name << " " << acc_num << " " << balance << endl;
     }
 };
 
 //Initialization
 int Customer::totalCust = 0;
+int Customer::totalBalance = 0;
 
 int main(){
     Customer A1("Tony", 1, 1000);
-    Customer A2("Thor", 2, 1500);
-    Customer A3("Rocky", 3, 896);
     A1.display();
+    Customer A2("Thor", 2, 1500);
     A2.display();
+    Customer A3("Rocky", 3, 896);
     A3.display();
+    Customer A4("sky", 4, 500);
+    A4.display();
+    Customer A5("Roc", 5, 600);
+    A5.display();
 
-    cout << "total customers: " << Customer::totalCust << endl; // access from anywhere
+    // cout << "total customers: " << Customer::totalCust << endl;    // access static data member directly from anywhere
+
+    Customer::getStaticData();
 }
